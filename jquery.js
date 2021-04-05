03c9440cb0edc4d029a4f1b3748a3b455a2eef14
Nick Rivadeneira <nickrivadeneira039@gmail>
2021-04-05 13:08:28 -0400

    commit

--- jquery.js
+++ jquery.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
 //Calculate the number of hours from now and a  given hour (in 24 hours format) of the day
 // If result is <0 means the given time is in the future
 // If result is >0 means the given time is in the past
+var pastTime = "true";
+var presentTime = "true";
+var futureTime = "true";
+
+
 function hoursFromNow(time) {
     //Create an instance of moment for the current time
     var now=moment();
@@ -13,3 +18,29 @@ function hoursFromNow(time) {
     //Return the difference in hours between the two moments
     return now.diff(todayTime,"hours");
   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //localStorage.setItem(".col-sm-11","#woot");
+
+$(".fa fa-save").click(function(){
+    row.push({
+        input:""
+    })
+    localStorage.setItem("saveButton",JSON.stringify("row"))
+});
